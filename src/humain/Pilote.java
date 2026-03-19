4f079c63da6cb20d5314fb31e7f9e20f5be5fb6f
Drekkou <stormix@yahoo.com>
2026-03-19 12:54:15 +0100

    debut de la gestion des voiles

--- src/humain/Pilote.java
+++ src/humain/Pilote.java
@@ -3,16 +3,18 @@ package humain;
 import materiel.*;
 import lieu.*;
 
-
-public class Pilote extends Humain{
+public class Pilote extends Humain {
     private Club club;
     private Parapente[] parapentes;
     private Sellette[] sellettes;
     private Parachute[] parachutes;
-    
+
     public Pilote(String nom, String prenom, String poids, Club club) {
         super(nom, prenom, poids);
         this.club = club;
+        this.parapentes = new Parapente[15];
+        this.sellettes = new Sellette[15];
+        this.parachutes = new Parachute[5];
     }
 
     public Club getClub() {
@@ -27,25 +29,23 @@ public class Pilote extends Humain{
         return parapentes;
     }
 
-    public void setParapentes(Parapente[] parapentes) {
-        this.parapentes = parapentes;
-    }
-
     public Sellette[] getSellettes() {
         return sellettes;
-    }
-
-    public void setSellettes(Sellette[] sellettes) {
-        this.sellettes = sellettes;
     }
 
     public Parachute[] getParachutes() {
         return parachutes;
     }
 
-    public void setParachutes(Parachute[] parachutes) {
-        this.parachutes = parachutes;
+    public void addParapente(Parapente parapente) {
+        this.parapentes[this.parapentes.length] = parapente;
     }
 
-    
+    public void addParapente(Parachute parachute) {
+        this.parachutes[this.parachutes.length] = parachute;
+    }
+
+    public void addParapente(Sellette sellette) {
+        this.sellettes[this.sellettes.length] = sellette;
+    }
 }
--- src/materiel/Parapente.java
+++ src/materiel/Parapente.java
@@ -2,7 +2,7 @@ package materiel;
 
 public class Parapente extends Materiel {
     private String nom;
-    private String[] daterevision;
+    private String[] dateRevision;
     private int tempsVolDerniereRevision;
     private int tempsVolTotal;
     private String classe;
@@ -14,7 +14,6 @@ public class Parapente extends Materiel {
         this.tempsVolDerniereRevision = tempsVolDerniereRevision;
         this.tempsVolTotal = tempsVolTotal;
         this.classe = classe;
-        this.daterevision = new String[50];
     }
 
     public String getNom() {
@@ -29,16 +28,32 @@ public class Parapente extends Materiel {
         return tempsVolDerniereRevision;
     }
 
-    public void setTempsVolDerniereRevision(int tempsVolDerniereRevision) {
-        this.tempsVolDerniereRevision = tempsVolDerniereRevision;
+    public void resetTempsVolDerniereRevision(){
+        this.tempsVolDerniereRevision=0;
+    }
+
+    public void initDateRevision(String[] tab){
+        this.dateRevision = tab;
+    }
+
+    public int ajoutDateRevision(String date){
+        int n =this.dateRevision.length;
+        if(n<50){
+            this.dateRevision[n] = date;
+            this.tempsVolDerniereRevision = 0;
+            return 0;
+        }else{
+            return 1;
+        }
     }
 
     public int getTempsVolTotal() {
         return tempsVolTotal;
     }
 
-    public void setTempsVolTotal(int tempsVolTotal) {
-        this.tempsVolTotal = tempsVolTotal;
+    public void addTempsVol(int tempsVol) {
+        this.tempsVolTotal += tempsVol;
+        this.tempsVolDerniereRevision += tempsVol;
     }
 
     public String getClasse() {
